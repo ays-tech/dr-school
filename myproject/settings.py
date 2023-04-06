96f3a8ead74e1f5f7290f42b24a0f03cfa720e6a
ays-tech <54658247+ays-tech@users.noreply.github.com>
2023-04-06 13:10:39 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     'widget_tweaks'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES   = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
